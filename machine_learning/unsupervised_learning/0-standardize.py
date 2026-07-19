1966aab3e308717af918410246235b73029f638a
Marcelo Rech <marcelorech@gmail.com>
2026-07-19 16:36:31 +0200

    Add task 0

--- machine_learning/unsupervised_learning/0-standardize.py
+++ machine_learning/unsupervised_learning/0-standardize.py
@@ -5,6 +5,9 @@ from sklearn import preprocessing
 
 
 def Standardize(X):
+    """Standardize tabular data using Scikit-learn's
+    StandardScaler to give each feature a mean of 0
+    and a standard deviation of 1."""
     scaler = preprocessing.StandardScaler()
     X_scaled = scaler.fit_transform(X)
     return X_scaled
