b655a3101d5b974ab7d76fdb76d195d46d264f27
Marcelo Rech <marcelorech@gmail.com>
2026-08-01 18:57:33 +0200

    Add tasks Linear Models

--- machine_learning/linear_models/5-logisitc_regression.py
+++ machine_learning/linear_models/5-logisitc_regression.py
@@ -7,5 +7,5 @@ from sklearn import linear_model
 def Logistic_Regression_Model(random_state):
     """Set up and return an untrained logistic regression model
     ready to handle binary classification tasks."""
-    model = linear_model.LogisticRegression(random_state = random_state)
+    model = linear_model.LogisticRegression(random_state=random_state)
     return model
