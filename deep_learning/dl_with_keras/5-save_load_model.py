b9c5e83ad39a9fd4a7f4eb03cca656573bf7f922
Marcelo Rech <marcelorech@gmail.com>
2026-08-12 18:21:46 +0200

    Tasks

--- deep_learning/dl_with_keras/5-save_load_model.py
+++ deep_learning/dl_with_keras/5-save_load_model.py
@@ -2,6 +2,7 @@
 """Introduction to Deep Learning with Keras.
 This module contains simple functions to build, train, and test
 neural networks using the Keras library."""
+from tensorflow import keras
 
 
 def save_model(model, filepath):
