475bd8d83b186e306ca78c771a393c43b547e56d
Marcelo Rech <marcelorech@gmail.com>
2026-08-22 13:27:45 +0200

    Add task

--- deep_learning/enhancing_dl_models/3-learning_rate_schedule.py
+++ deep_learning/enhancing_dl_models/3-learning_rate_schedule.py
@@ -14,12 +14,14 @@ def get_optimizer_SGD_with_schedule(
         lr_schedule = keras.optimizers.schedules.ExponentialDecay(
             initial_learning_rate=initial_lr,
             decay_steps=decay_steps,
-            decay_rate=decay_rate)
+            decay_rate=decay_rate,
+            staircase=True)
     elif schedule_type == "inverse_time":
         lr_schedule = keras.optimizers.schedules.InverseTimeDecay(
             initial_learning_rate=initial_lr,
             decay_steps=decay_steps,
-            decay_rate=decay_rate)
+            decay_rate=decay_rate,
+            staircase=True)
 
     optimizer = keras.optimizers.SGD(
         learning_rate=lr_schedule, momentum=momentum)
