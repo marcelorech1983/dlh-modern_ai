1080de9e91e4cfc521431d328496e434b5cd5f6a
Marcelo Rech <marcelorech@gmail.com>
2026-09-01 18:22:25 +0200

    Add Tasks

--- deep_learning/cv_architectures/0-create_cnn_model.py
+++ deep_learning/cv_architectures/0-create_cnn_model.py
@@ -16,9 +16,9 @@ def create_cnn_model(input_shape, filters, kernel_sizes,
         model.add(keras.layers.Conv2D(filters=f, kernel_size=k,
                                       activation=a))
         if pooling_type == 'max':
-            model.add(keras.layers.MaxPooling2D())
+            model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
         elif pooling_type == 'avg':
-            model.add(keras.layers.AveragePooling2D())
+            model.add(keras.layers.AveragePooling2D(poll_size=(2, 2)))
 
     model.add(keras.layers.Flatten())
     model.add(keras.layers.Dense(10, activation='softmax'))
