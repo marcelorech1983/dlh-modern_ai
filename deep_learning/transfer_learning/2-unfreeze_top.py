839374f7e074b4e4c1e14b4b1107cc8ef8705107
Marcelo Rech <marcelorech@gmail.com>
2026-09-05 16:44:46 +0200

    Add Tasks

--- deep_learning/transfer_learning/2-unfreeze_top.py
+++ deep_learning/transfer_learning/2-unfreeze_top.py
@@ -3,8 +3,6 @@
 This module provides functions for feature extraction, model fine-tuning,
 and building transfer learning pipelines using pretrained CNNs in Keras."""
 
-from tensorflow import keras
-
 
 def unfreeze_top_layers(model, n_layers):
     """Unfreezes the last n layers of a base model for fine-tuning."""
